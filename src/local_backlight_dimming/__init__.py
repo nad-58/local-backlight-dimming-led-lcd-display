44f9ef9b78c1ecf4c88c3871e26aeb0bbdb619c5
nad-58 <nadernejad.ehsan@gmail.com>
2026-05-28 19:54:10 +0100

    Expose converted Python algorithms

--- src/local_backlight_dimming/__init__.py
+++ src/local_backlight_dimming/__init__.py
@@ -3,21 +3,41 @@
 from .display_model import DisplayModel
 from .algorithms import (
     average_dimming,
-    maximum_dimming,
-    sqrt_dimming,
+    average_plus_dimming,
+    chen_dimming,
     cho_dimming,
+    conventional_dimming,
+    local_compensation_cho,
+    local_compensation_nam,
+    matlab_ehsan_variant,
+    maximum_dimming,
     nam_dimming,
     proposed_histogram_dimming,
+    sqrt_dimming,
 )
 from .compensation import compensate_lcd
+from .optimization import OptimizationParams, clipper_free_dimming, fast_refinement, new_dimming
+from .result import RenderResult, render_result
 
 __all__ = [
     "DisplayModel",
+    "RenderResult",
+    "OptimizationParams",
     "average_dimming",
-    "maximum_dimming",
-    "sqrt_dimming",
+    "average_plus_dimming",
+    "chen_dimming",
     "cho_dimming",
+    "conventional_dimming",
+    "local_compensation_cho",
+    "local_compensation_nam",
+    "matlab_ehsan_variant",
+    "maximum_dimming",
     "nam_dimming",
     "proposed_histogram_dimming",
+    "sqrt_dimming",
+    "clipper_free_dimming",
+    "fast_refinement",
+    "new_dimming",
     "compensate_lcd",
+    "render_result",
 ]
